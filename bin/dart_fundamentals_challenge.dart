9ba8a4cffb0ecb46c7a121e6028ec590f8631f7e
Fernando Silva <fernnlcs@gmail.com>
2023-02-24 02:18:59 -0300

    first commit

---
+++ bin/dart_fundamentals_challenge.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fundamentals_challenge/dart_fundamentals_challenge.dart' as dart_fundamentals_challenge;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fundamentals_challenge.calculate()}!');
+}
---
+++ test/dart_fundamentals_challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fundamentals_challenge/dart_fundamentals_challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
